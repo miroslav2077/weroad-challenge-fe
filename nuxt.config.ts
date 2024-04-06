d7ceb057777d5b3a306263599f382837f2f40fb2
Mirko Grasso <mirko.grasso@gmbfinance.eu>
2024-04-06 20:12:05 +0200

    removing apollo as a dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/apollo', 'nuxt-graphql-client'],
+  modules: ['nuxt-graphql-client'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
